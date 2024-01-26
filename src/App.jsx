015d1efc42f0276482d80989619e5ce90f297357
FrowningCat <vsavinkov60@gmail.com>
2024-01-26 14:31:35 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h1>Hello React</h1>
+            </main>
+        </div>
+    );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <h3>Test vite</h3>
+
+            <span>time</span>
+        </header>
+    );
+}
