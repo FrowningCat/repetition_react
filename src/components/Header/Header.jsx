5c615f7e48fe862754b785181fafb5b4371a180c
FrowningCat <vsavinkov60@gmail.com>
2024-01-30 14:10:43 +0300

    styled-components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,16 @@
 import logo from '/vite.svg';
 import { useState } from 'react';
-import classes from './Header.module.css';
+import { styled } from 'styled-components';
+
+const HeaderContainer = styled.header`
+    height: 50px;
+    display: flex;
+    padding: 0 2rem;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
+    background: #fafafa;
+`;
 
 export default function Header() {
     const [now, setNow] = useState(new Date());
@@ -9,10 +19,10 @@ export default function Header() {
     setInterval(() => setNow(new Date()), 1000);
 
     return (
-        <header>
+        <HeaderContainer>
             <img src={logo} alt={name} />
 
             <span>Время сейчас: {now.toLocaleTimeString()}</span>
-        </header>
+        </HeaderContainer>
     );
 }
