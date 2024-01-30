2e82b581101aa660e83383d8e66c05a0e1277b83
FrowningCat <vsavinkov60@gmail.com>
2024-01-30 13:53:32 +0300

    interactive clicks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import TeachingSection from './components/TeachingSection';
 import DifferencesSection from './components/DifferencesSection';
 import IntroSetion from './components/IntroSection';
+import TabsSection from './components/TabsSection';
+import FeedbackSection from './components/FeedbackSection';
 
 export default function App() {
+    const [tab, setTab] = useState('feedback');
+
     return (
         <>
             <Header />
             <main>
                 <IntroSetion />
-                <TeachingSection />
-                <DifferencesSection />
+                <TabsSection
+                    active={tab}
+                    onChange={(current) => setTab(current)}
+                />
+
+                {tab == 'main' && (
+                    <>
+                        <TeachingSection />
+                        <DifferencesSection />
+                    </>
+                )}
+
+                {tab == 'feedback' && <FeedbackSection />}
             </main>
         </>
     );
